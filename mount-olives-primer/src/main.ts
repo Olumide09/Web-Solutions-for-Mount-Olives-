70996fa1cb0d98159f13fd91e997ceb6006f0bab
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-04-15 17:17:01 +0100

    Update main.ts

--- mount-olives-primer/src/main.ts
+++ mount-olives-primer/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as nunjucks from 'nunjucks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //create a Nest application with Express under the hood
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  /*To associate nunjucks with express,
+we need to get the underlying express platform from Nest
+app */
+  const express = app.getHttpAdapter().getInstance();
+  /*We also need to get directory name views (create it in
+  project root directory),
+  which is the root directory for our template files*/
+  const views = join(__dirname, '..', 'views');
+  /*Finally, configure nunjucks, setting views and express
+  declared above*/
+  nunjucks.configure(views, { express });
+  //start the application
   await app.listen(3005);
 }
 bootstrap();
